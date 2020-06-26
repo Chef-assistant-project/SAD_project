7aba84dd0930be02e80fdcc2612cc7bba9dbe2d0
maryam <vaeztmaryam@gmail.com>
2020-06-26 13:54:34 +0430

    UpdateView for email and password

--- users/forms.py
+++ users/forms.py
@@ -13,6 +13,46 @@ class UserRegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ['email']
+
+
+class UpdatePasswordForm(forms.Form):
+    current = forms.CharField(label='Current password',
+                              widget=forms.PasswordInput)
+
+    password1 = forms.CharField(label='New password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation',
+        help_text='Same password as above.',
+        widget=forms.PasswordInput,
+    )
+    def __init__(self, *args, **kwargs):
+        self.instance = kwargs.pop('instance', None)
+        assert self.instance is not None
+        super(UpdatePasswordForm, self).__init__(*args, **kwargs)
+
+    def clean_current(self):
+        password = self.cleaned_data['current']
+
+        assert self.instance is not None
+        if not self.instance.check_password(password):
+            raise forms.ValidationError("The current password was invalid.")
+
+        return password
+
+    def clean(self):
+        cleaned_data = super(UpdatePasswordForm, self).clean()
+        if cleaned_data['password1'] != cleaned_data['password2']:
+            raise forms.ValidationError("Passwords didn't match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        assert self.instance is not None
+        self.instance.set_password(self.cleaned_data['password1'])
+        if commit:
+            self.instance.save()
+        return self.instance
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import User
+from .forms import UserRegisterForm, UserUpdateForm, UpdatePasswordForm
 
 
 def register(request):
@@ -19,33 +20,27 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
 @login_required
-def UpdateUser(request, typeOfRequest):
-    methodIsPost = False
-    if typeOfRequest == "profile":
-        return render(request, 'users/profile.html')
-    elif typeOfRequest == "changeEmail":
-        if request.method == 'POST':
-            methodIsPost = True
-            form = UserUpdateForm(request.POST, instance=request.user)
-    elif typeOfRequest == "changePassword":
-        if request.method == 'POST':
-            methodIsPost = True
-            form = PasswordChangeForm(data=request.POST, user=request.user)
-    if methodIsPost:
-        if form.is_valid():
-            form.save()
-            if typeOfRequest == "changePassword":
-                update_session_auth_hash(request, form.user)
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('profile' ,typeOfRequest = "profile")
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        if typeOfRequest == "changeEmail":
-            form = UserUpdateForm(instance=request.user)
-        else:
-            form = PasswordChangeForm(request.user)
-    if typeOfRequest == "changeEmail":
-        return render(request, 'users/changeEmail.html', {'form': form})
-    return render(request, 'users/changePassword.html', {'form': form})
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+class ChangeEmail(generic.UpdateView):
+    model = User
+    fields = ['email']
+    template_name = 'users/changeEmail.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ChangePassword(generic.UpdateView):
+    model = User
+    template_name = 'users/changePassword.html'
+    success_url = reverse_lazy('login')
+    form_class = UpdatePasswordForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
